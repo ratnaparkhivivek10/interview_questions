413091039bd98d1b7196314b3b203e62bcd4f7bc
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-03-12 23:34:14 +0530

    avantari interview assignment

---
+++ avantari/ml_test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def water_stored_in_platform(platform):
+    pass
+
+    
--- crowdstrike/interview_questions.py
+++ crowdstrike/interview_questions.py
@@ -1,26 +1,26 @@
-# square the elements in the list
+#Q1 - square the elements in the list
 data = [1, 2, 3]
 # solution
 data_squared = [i*i for i in data]
 data_squared = list(map(lambda x: x*x, data))
 
-# sort below list
+#Q2 - sort below list
 data = [(1,2), (2,3), (4,1)]
 # solution
 data_sorted = sorted(data, key=lambda x: x[1])
 
-# create generator to square the elements from 0-10
+#Q3 - create generator to square the elements from 0-10
 # solution
 def square_elements():
     for i in range(10):
         yield i*i
 
-# find smallest common elements from below three list, don't use pythonic features
+#Q4 - find smallest common elements from below three list, don't use pythonic features
 a = [1,2,3,4,5]
 b = [2,3,7,8]
 c = [3,9]
 
-# solution - return smallest single element
+#Q5 - solution - return smallest single element
 def smallest_common_min_element(list_1, list_2, list_3):
     # create pointers for each list
     i, j, k = 0, 0, 0
@@ -78,5 +78,5 @@ print(smallest_common_min_element(a, b, c))
 # try second solution
 common_elements(a, b, c)
 
-# how will you read file having one million urls
-# design database tables showing many to many relationship between books and authors, one book can have many authors and an author can have many books
+#Q6 - how will you read file having one million urls
+#Q7 - design database tables showing many to many relationship between books and authors, one book can have many authors and an author can have many books
